a2b2a821b33853385ae597acfd2f3cf657259872
Marco 9726 <marcosarra9@gmail.com>
2023-05-04 15:29:55 +0200

    esercizio svolto

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,7 +1,32 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
+
 public class CalcolaBiglietto {
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Numeri di km da percorrere: ");
+		int km = Integer.parseInt(in.nextLine());
+		System.out.println(km);
+		
+		System.out.print("Età: ");
+		int età = Integer.parseInt(in.nextLine());
+		System.out.println(età);
+//		
+		double prezzoBase = km * 0.21;
+		System.out.print("Prezzo base: " + prezzoBase);
+		
+		if ( età < 18) {
+//			sottraggo al prezzo base il 20% dello stesso
+			double prezzoScontato = prezzoBase - prezzoBase / 5;
+			System.out.print("prezzo scontato: " + prezzoScontato);
+			
+		} else if (età > 65 ) {
+//			sottraggo al prezzo base il 40% dello stesso
+			double prezzoScontato = prezzoBase - (prezzoBase / 5) * 2;
+			System.out.print("prezzo scontato: " + prezzoScontato);
+		}
 		
 	}
 }
