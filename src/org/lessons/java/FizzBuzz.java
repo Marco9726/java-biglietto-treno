175113ace452b1ff327de753a6ed9a6c69bbb942
Marco 9726 <marcosarra9@gmail.com>
2023-05-04 17:20:18 +0200

    bonus 1

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -12,9 +12,27 @@ public class FizzBuzz {
 			} else if (i % 3 == 0) {
 				System.out.println("Fizz");
 			} else {
-				System.out.println(i);
-				
+				System.out.println(i);	
 			}
+		}
+		
+		System.out.println("---------------");
+		
+		//BONUS
+		int x = 1;
+		while ( x <= 100) {
+			if ( x % 3 == 0 && x % 5 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (x % 5 == 0) {
+				System.out.println("Buzz");
+			} else if (x % 3 == 0) {
+				System.out.println("Fizz");
+			} else {
+				System.out.println(x);	
+			}
+			++x;
 		}
 	}
 }
+//		
+//	}
